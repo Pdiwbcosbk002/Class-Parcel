7c1142c4a4729c9bf20cceae8e0d2130e6d72e74
Pdiwbcosbk002 <137616471+Pdiwbcosbk002@users.noreply.github.com>
2024-04-19 14:45:04 +0700

    index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+require_once 'class.php';
+// Code BY https://www.facebook.com/profile.php?id=100053315620756&mibextid=ZbWKwL
+// ขอความร่วมมือ อย่าเอาโค้ดไปขายครับ
+
+// --------------- ทดสอบ -------------- //
+
+// J&T
+//echo JT_Express('หมายเลขพัสดุ', 'เลขท้าย 4 ตัว เบอร์ผู้รับ');
+
+// Flash_Express
+//echo Flash_Express('หมายเลขพัสดุ');
+
+// SPX_Express
+//echo SPX_Express('หมายเลขพัสดุ');
+
+// TNT_Express
+//echo TNT_Express('หมายเลขพัสดุ');
+
+// Code BY https://www.facebook.com/profile.php?id=100053315620756&mibextid=ZbWKwL
+// ขอความร่วมมือ อย่าเอาโค้ดไปขายครับ
+?>
